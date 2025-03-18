4bebe4a745e973743d006d3a7671bc58a53bf8e4
reyhan2498 <reyhan@megatransact.com>
2025-03-18 16:20:56 +1300

    icons added

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -1,19 +1,28 @@
 import React from "react";
+import "./intro.css";
+import Github from "../../img/github.png";
+import Linkedin from "../../img/linkedin.png";
+import Instagram from "../../img/instagram.png";
 
 const Intro = () => {
 return (
     <div className="i-wrapper">
         <div className="i-left">
             <div className="i-name">
-                <span>Hi There, I'm</span>
+                <span>Hey I'm</span>
                 <span>Alex</span>
                 <span>Frontend Developer</span>
             </div>
             <button className="button i-button">Hire me</button>
-        
+            <div className="i-icons">
+                <img src={Github} alt="" />
+                <img src={Linkedin} alt="" />
+                <img src={Instagram} alt="" />
+            </div>
         </div>
         <div className="i-right">
-            <img src="https://images.unsplash.com/photo-1622835021267-0d4f9c6b2d6b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="Alex" />
+
+            I am on the right side
         </div>
     </div>
 )
