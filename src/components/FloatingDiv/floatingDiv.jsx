d62bda4cfc26dcb5983a929c8460b108fbfe5c04
reyhan2498 <reyhan@megatransact.com>
2025-03-19 16:01:14 +1300

    completed implementing

--- src/components/FloatingDiv/floatingDiv.jsx
+++ src/components/FloatingDiv/floatingDiv.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-import "./floatingDiv.css";
 
-const FloatingDiv = ({image, txt1, txt2}) => {
-    return (
-        <div className="floatingDiv">
-            <img src={image} alt="" />
-            <span>{txt1} <br/>{txt2}</span>
-        </div>          
-    )            
-}
+import './FloatingDiv.css'
 
-export default FloatingDiv;
+
+const FloatinDiv = ({img, text1, text2}) => {
+  return (
+    // darkMode
+    <div className="floatingDiv">
+      <img src={img} alt="" />
+      <span>
+        {text1}
+        <br/>
+        {text2}
+      </span>
+    </div>
+  );
+};
+
+export default FloatinDiv;
--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,29 +1,52 @@
-import React from 'react';
-import './navbar.css';
-
-const Navbar = () => {
-    return (
-        <div className="n-wrapper">
-            <div className="n-left">
-                <div className="n-name">Christopher</div>
-                <span>toggle</span>
-                <div className="n-right">
-                    <div className="n-list">
-                        <ul style={{listStyleType:'none'}}>
-                            <li>Home</li>
-                            <li>Services</li>
-                            <li>Experiences</li>
-                            <li>Portfolio</li>
-                            <li>Testimonials</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <button className='button n-button'>Contact Us</button>
-                    </div>
-                </div>         
-            </div>
+import React from "react";
+import Toggle from "../Toggle/Toggle";
+import "./Navbar.css";
+import { Link } from "react-scroll";
+const navbar = () => {
+  return (
+    <div className="n-wrapper" id="Navbar">
+      {/* left */}
+      <div className="n-left">
+        <div className="n-name">Andrew</div>
+        <Toggle />
+      </div>
+      {/* right */}
+      <div className="n-right">
+        <div className="n-list">
+          <ul style={{ listStyleType: "none" }}>
+            <li>
+              <Link activeClass="active" to="Navbar" spy={true} smooth={true}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="services" spy={true} smooth={true}>
+                Serivces
+              </Link>
+            </li>
+            <li>
+              <Link to="works" spy={true} smooth={true}>
+                Experience
+              </Link>
+            </li>
+            <li>
+              <Link to="portfolio" spy={true} smooth={true}>
+                Protfolio
+              </Link>
+            </li>
+            <li>
+              <Link to="testimonial" spy={true} smooth={true}>
+                Testimonial
+              </Link>
+            </li>
+          </ul>
         </div>
-    );
+        <Link to="contact" spy={true} smooth={true}>
+        <button className="button n-button">Contact</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
-export default Navbar;
+export default navbar;
